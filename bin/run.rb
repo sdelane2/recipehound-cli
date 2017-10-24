85eea901259e406c4362748746087072d4d165c8
github username <github email address>
2017-10-24 14:23:06 -0400

    Started welcome methods

--- bin/run.rb
+++ bin/run.rb
@@ -1,36 +1,43 @@
-#require_relative '../config/environment'
-#require_relative '../lib/api_communicator'
-
+# #require_relative '../config/environment'
+# #require_relative '../lib/api_communicator'
+#
 def welcome_message
   puts "Welcome to RecipeHound, the CLI app that sniffs out the perfect recipe for you!"
 end
 
 
-def create_new_account_or_login
+def create_new_account_or_login_prompt
   puts "Would you like to create a new account or sign in to an existing account?"
   puts "Enter '1' to create a new account. Enter '2' to sign in to an existing account."
-  if valid_welcome_input?
-    welcome_option
-  else
-    welcome_try_again
-  end
-end
 
 
-
+  # until valid_welcome_input? == true
+  #   welcome_try_again
+  # end
+  #   welcome_option
 
 
   # user_welcome_input = gets.chomp.to_i
-  # if user_welcome_input == 1
-  #   puts "Great! Let's creat a new account for you!"
-  # elsif user_welcome_input == 2
-  #   puts "Awesome! Let's create a new account for you!"
+  # if valid_welcome_input?
+  #   welcome_option
   # else
-  #   puts "That is not a valid option. Please enter '1' to create a new account or enter '2' to sign in to an existing account."
+  #   welcome_try_again
   # end
-#  end
-#end
 
+
+end
+
+#   # user_welcome_input = gets.chomp.to_i
+#   # if user_welcome_input == 1
+#   #   puts "Great! Let's creat a new account for you!"
+#   # elsif user_welcome_input == 2
+#   #   puts "Awesome! Let's create a new account for you!"
+#   # else
+#   #   puts "That is not a valid option. Please enter '1' to create a new account or enter '2' to sign in to an existing account."
+#   # end
+# #  end
+# #end
+#
 def valid_welcome_input?
   user_welcome_input = gets.chomp.to_i
   if user_welcome_input == 1
@@ -43,25 +50,24 @@ def valid_welcome_input?
 end
 
 def welcome_option
-
   if user_welcome_input == 1
-    puts "Great! Let's creat a new account for you!"
+    puts "Great! Let's create a new account for you!"
   else user_welcome_input == 2
     puts "Awesome! Let's create a new account for you!"
   end
 end
 
 def welcome_try_again
-  until valid_welcome_input?
     puts "That is not a valid option. Please enter '1' to create a new account or enter '2' to sign in to an existing account."
     user_welcome_input = gets.chomp.to_i
-  end
 end
 
 def create_new_user_account
-
+  puts "Great! Let's create a new account for you!"
+  puts "What's your first name?"
+  user_first_name = gets.chomp
 end
 
 
 welcome_message
-create_new_account_or_login
+create_new_account_or_login_prompt
