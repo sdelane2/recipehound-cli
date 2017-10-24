49bbb18a8fa588d2660fcf716b3d56b08259c83a
yassimortensen <yassimortensen@gmail.com>
2017-10-24 14:34:38 -0400

    iterated through array of hashes to create db of recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require_relative "../config/environment.rb"
 
 Ingredient.delete_all
 RecipeIngredient.delete_all
@@ -6,3 +7,7 @@ User.delete_all
 UserRecipe.delete_all
 
 array_of_hashes = get_all_recipes
+
+array_of_hashes.each do |hash|
+  Recipe.find_or_create_by(hash)
+end
