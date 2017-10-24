5864ebfa6b7e1703e9d7228168811f86ea244ab5
github username <github email address>
2017-10-24 14:35:00 -0400

    Added prompts to create new user

--- bin/run.rb
+++ bin/run.rb
@@ -66,8 +66,17 @@ def create_new_user_account
   puts "Great! Let's create a new account for you!"
   puts "What's your first name?"
   user_first_name = gets.chomp
+  puts "Hi, #{user_first_name}. What would you like your username to be?"
+  user_username = gets.chomp
+  # => INSERT A FIND BY USERNAME METHOD
+  puts "Got it. Choose a password. Make sure you save it in a safe place!"
+  user_password = gets.chomp
+  puts "Great! You're all set up, #{user_first_name}! Your username is #{user_username}."
+  # => username = User.create(user_first_name, user_username, user_password)
 end
 
 
-welcome_message
-create_new_account_or_login_prompt
+# welcome_message
+# create_new_account_or_login_prompt
+
+create_new_user_account
