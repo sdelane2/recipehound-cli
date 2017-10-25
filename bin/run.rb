bf5abdbc77eca7595f76504763d9c699dd4dd30a
github username <github email address>
2017-10-25 13:15:38 -0400

    added gems rmagick

--- bin/run.rb
+++ bin/run.rb
@@ -1,10 +1,11 @@
 require_relative '../config/environment'
 require_relative '../lib/api_communicator'
 require_relative '../lib/cli'
+require 'asciiart'
 
-
-
+#ascii = ASCII_Image.new("http://www.dogingtonpost.com/wp-content/uploads/2016/11/cookingwithdog.jpg")
+#ascii.build(20)
  #welcome_message
  #create_new_account_or_login_prompt
 
-find_recipe_by_ingredient
+#find_recipe_by_ingredient
