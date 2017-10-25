a9d9740a2fb0a6999a64ddb9e6bfd9f8b5f94b22
github username <github email address>
2017-10-25 13:26:36 -0400

    added ascii art function

--- bin/run.rb
+++ bin/run.rb
@@ -3,9 +3,10 @@ require_relative '../lib/api_communicator'
 require_relative '../lib/cli'
 require 'asciiart'
 
-#ascii = ASCII_Image.new("http://www.dogingtonpost.com/wp-content/uploads/2016/11/cookingwithdog.jpg")
-#ascii.build(20)
- #welcome_message
- #create_new_account_or_login_prompt
+#ascii = AsciiArt.new("http://www.reactiongifs.com/r/dgc.gif")
+#print ascii.to_ascii_art
+
+#welcome_message
+#create_new_account_or_login_prompt
 
 #find_recipe_by_ingredient
