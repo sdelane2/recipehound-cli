55e6fdb275eaf24843212d463bf11694f2d2fd46
github username <github email address>
2017-10-25 18:08:17 -0400

    completed cli saved_recipe

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,3 +1,6 @@
+require 'pry'
+
+
 def welcome_message # => greets user at the start of the program
   puts "Welcome to RecipeHound, the CLI app that sniffs out the perfect recipe for you!"
 end
@@ -14,53 +17,56 @@ def welcome_option  # => validation for user input on create_new_account_or_logi
   user_welcome_input = gets.chomp.to_i
   if user_welcome_input == 1
     puts "Awesome! Let's create a new account for you!"
-    create_new_user_account
+    user = create_new_user_account
   elsif user_welcome_input == 2
-    existing_account_login
+    user = existing_account_login
   else
-    puts "That is not a valid option. Please enter '1' to create a new account or enter '2' to sign in to an existing account."
+    puts "Ruh roh! That is not a valid option. Please enter '1' to create a new account or enter '2' to sign in to an existing account."
     welcome_option
   end
+  user
 end
 
 
-def create_new_user_account # => takes user inputs and creates a new User instance with those a
-  # puts "Awesome! Let's create a new account for you!"
+def create_new_user_account # => takes user inputs and creates a new User instance
   puts "What would you like your username to be?"
-  user_username = gets.chomp
-  taken_username = User.find_by(username: user_username)
-  if taken_username == nil
+  user_username_input = gets.chomp
+  user = User.find_by(username: user_username_input)
+  if user == nil
+    puts "I like that. What's your first name?"
+    user_first_name = gets.chomp
     puts "Got it. Choose a password. Make sure you save it in a safe place!"
     user_password = gets.chomp
-    puts "What's your first name?"
-    user_first_name = gets.chomp
-    puts "You're all set up, #{user_first_name}! Your username is #{user_username}."
-    user_username = User.create(first_name: user_first_name, username: user_username, password: user_password)
+    puts "You're all set up, #{user_first_name}! Your username is #{user_username_input}."
+    user = User.create(first_name: user_first_name, username: user_username_input, password: user_password)
   else
     puts "Sorry, that username is already taken."
     create_new_user_account
   end
+  user
 end
+
 
 def existing_account_login
   puts "Please enter your username."
   user_username = gets.chomp
-  existing_user = User.find_by(username: user_username)
-  if existing_user == nil
-    puts "No user found with that username. Please try again."
+  user = User.find_by(username: user_username)
+  if user == nil
+    puts "Ruh roh! No user found with that username. Please try again."
   else
-    puts "Hello #{existing_user.first_name}. Welcome back!"
-    existing_user
+    puts "Hello #{user.first_name}. Welcome back!"
   end
+user
 end
 
-def find_recipe_by_ingredient
+
+def find_recipe_by_ingredient(user)
   puts "What ingredient would you like to use?"
   user_ingredient = gets.chomp
   ingredient_given = Ingredient.find_by(title: user_ingredient)
   if ingredient_given == nil
-    puts "No recipes with that ingredient could be found. Try making it plural?"
-    find_recipe_by_ingredient
+    puts "Ruh roh! No recipes with that ingredient could be found. Try making it plural?"
+    find_recipe_by_ingredient(user)
   else
     relationships = RecipeIngredient.where(ingredient_id: ingredient_given.id)
     recipe_ids = relationships.collect {|row| row.recipe_id}
@@ -71,29 +77,30 @@ def find_recipe_by_ingredient
     recipes.each do |recipe|
       puts "#{recipe.title}"
     end
-    save_recipe
   end
 end
 
-def save_recipe
+
+def save_recipe(user)
   puts "Would you like to save a recipe from this list? (y/n)"
-  should_recipe_be_saved = gets.chomp.downcase
-    if should_recipe_be_saved == "n"
+  should_recipe_be_saved = gets.chomp
+    if should_recipe_be_saved != "n" && should_recipe_be_saved != "y"
+      puts "Get it together, #{user.first_name}. That's not a valid input."
+      save_recipe(user)
+    elsif should_recipe_be_saved == "n"
       false
-    elsif should_recipe_be_saved == "y"
+    else should_recipe_be_saved == "y"
       puts "Okay. Enter the name of the recipe you'd like to save."
       saved_recipe_input = gets.chomp
       saved_recipe = Recipe.find_by(title: saved_recipe_input)
-      new_user_recipe = UserRecipe.new(recipe_id: saved_recipe.id, user_id: self.id)
-    else
-      puts "Get it together, #{user.first_name}. That's not a valid input."
-      saved_recipe
+      new_user_recipe = UserRecipe.create(recipe: saved_recipe, user: user)
+      puts "Great! #{saved_recipe_input} has been saved to your list."
     end
 end
 
 
 
-def shopping_list
+def shopping_list(user)
   puts "Shopping List"
 
 end
