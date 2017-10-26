c83ae3fd5929164394e93946bb3a94607d56af89
github username <github email address>
2017-10-26 12:52:26 -0400

    added and fixed prompt_menu

--- bin/run.rb
+++ bin/run.rb
@@ -3,19 +3,28 @@ require_relative '../lib/api_communicator'
 require_relative '../lib/cli'
 require 'asciiart'
 require 'tty-prompt'
+# require 'figlet'
+#
+# font = Figlet::Font.new
+# figlet = Figlet::Typesetter.new(font)
+# puts figlet['hello world']
 
 
-# => USE THIS ONE
-ascii = AsciiArt.new("http://c1.staticflickr.com/5/4468/37882203826_01aeb6ea0c_b.jpg")
-print ascii.to_ascii_art
 
 
- #welcome_message
-
- #user = create_new_account_or_login_prompt
-
- # => insert menu options
- #find_recipe_by_ingredient(user)
-
- #save_recipe(user)
- prompt_menu
+# # => USE THIS ONE
+# ascii = AsciiArt.new("http://c1.staticflickr.com/5/4468/37882203826_01aeb6ea0c_b.jpg")
+# print ascii.to_ascii_art
+#
+#
+  welcome_message
+#
+  user = create_new_account_or_login_prompt
+#fetch_user_recipes(user)
+prompt_menu(user)
+#
+#  # => insert menu options
+#  #find_recipe_by_ingredient(user)
+#
+#  #save_recipe(user)
+#  prompt_menu
